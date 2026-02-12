73fea0a39e480323734a25d8a2652e1ccebe298c
Skilzy <skilzyafk@gmail.com>
2026-02-12 15:06:10 +0200

    Initial commit

---
+++ starter/script.js
@@ -0,0 +1,106 @@
+/*
+
+let js = "amazing";
+
+if (js === "amazing") alert ("java Is Fun");
+
+40 + 8 + 23 - 10;
+ 
+console.log( 40 + 8 + 23 -10);
+const ageSarah = 45; 
+
+const isFullAge = ageSarah >=46;  
+console.log(isFullAge);
+
+const year = 2025;
+
+const age =  year - 2005;
+
+const job = 'Student';
+
+const n1ame = 'Mohammed'; 
+
+console.log(`I'm ${n1ame} I'm ${age} Years Old And I'm A ${job}`); 
+
+
+
+
+if (age >= 18) { console.log(`u are logged in`); 
+     
+    
+} else {console.log( `can't log in `)}
+
+
+const nuns = Number(prompt("what is happening"))
+if (nuns === 20) {
+
+    console.log('so?')
+
+}
+   else if (nuns !== 20) {
+  
+    console.log ('what')
+}  
+
+
+// */
+// const him = true ;
+// const he = false;
+// console.log(him && him); 
+// console.log(he && him);
+// console.log(he && he);
+// console.log(him || he   );
+// console.log(him || him);
+/*
+const Dolphins = (1+ 110 + 90) / 3;
+
+console.log(Dolphins);
+
+const Koalas = (80 +101+110) / 3 ;
+
+console.log(Koalas);
+
+
+if ( Koalas > Dolphins && Koalas >= 100 ) {
+    console.log( Koalas +" koalas won the trophy ");
+
+} else if (Koalas < Dolphins && Dolphins >= 100 ) 
+    { console.log(Dolphins + " Dolphins won the trophy" );
+    
+} else if (Koalas === Dolphins && Dolphins >= 100 && Koalas >=100)
+     { console.log("Both Won The Trophy");
+    
+} else { console.log("NoBody Won The Trophy");
+    
+}
+ 
+ 
+const day = "thursday";
+    switch (day) {
+        case  'monday': 
+        console.log("playing football");
+        break; 
+        case 'sunday':
+        case "tuesday":
+        console.log('shut the hell up');
+break;
+        case "thursday":
+            console.log("exercising");         }
+
+*/
+
+const bill_value = 301;  
+
+const tip = bill_value >=50 && bill_value <=300 ? bill_value * 0.15 : bill_value*0.2;
+
+console.log( bill_value + tip ); 
+ 
+const age = 23; 
+
+age >=18 && age <= 65 ? console.log("u r eligible to work") : console.log("u r not eligible to work");
+
+const bill_value = 40;
+
+const tip = bill_value >=50 && bill_value <=300 ? bill_value * 0.15 : bill_value*0.2;
+console.log(`the tip is ${tip} bill value is ${bill_value} total value is ${bill_value + tip}`);
+console.log(bill_value + tip + " is the total value including tip");
